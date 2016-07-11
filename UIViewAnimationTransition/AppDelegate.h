082010ef3c5a6acf03109f2a8c8352068ba838e0
Cantoraz Chou <cantoraz@gmail.com>
2016-07-11 14:03:26 +0800

    Initial Commit

---
+++ UIViewAnimationTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewAnimationTransition
+//
+//  Created by Cantoraz Chou on 7/11/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewAnimationTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewAnimationTransition
+//
+//  Created by Cantoraz Chou on 7/11/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
